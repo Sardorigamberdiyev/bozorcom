bc8624b4f1c363e33a7b0a11ab43b4fa0cdb554a
Sardorigamberdiyev <72994594+Sardorigamberdiyev@users.noreply.github.com>
2020-12-06 13:37:50 +0500

    Updates

---
+++ precache-manifest.f32dd2d096275fdfa5258a80e4798e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855d8e1fc9b3412d755668b7e5e78087",
+    "url": "/bozorcom/index.html"
+  },
+  {
+    "revision": "c076aea4f6ddfbc8b97d",
+    "url": "/bozorcom/static/css/2.ce773819.chunk.css"
+  },
+  {
+    "revision": "02f0fdb46d279eb99902",
+    "url": "/bozorcom/static/css/main.4d832a7c.chunk.css"
+  },
+  {
+    "revision": "c076aea4f6ddfbc8b97d",
+    "url": "/bozorcom/static/js/2.acde7f3f.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/bozorcom/static/js/2.acde7f3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f0fdb46d279eb99902",
+    "url": "/bozorcom/static/js/main.e3be118a.chunk.js"
+  },
+  {
+    "revision": "4fe2592fa1d8b5a340e3",
+    "url": "/bozorcom/static/js/runtime-main.25776683.js"
+  }
+]);
