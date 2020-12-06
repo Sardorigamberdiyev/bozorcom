138fd8f75bfbba6a3dcf3c0959b279db53752e9b
Sardorigamberdiyev <72994594+Sardorigamberdiyev@users.noreply.github.com>
2020-12-06 14:14:41 +0500

    Updates

--- precache-manifest.6dd9560fe949ca5ae8ff61d230dafc82.js
+++ precache-manifest.6dd9560fe949ca5ae8ff61d230dafc82.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e13ea2628080717bf87b10b1bdac8f6e",
+    "revision": "fdac2e5c0616c29acc3321541fc0247e",
     "url": "/bozorcom/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/bozorcom/static/css/2.ce773819.chunk.css"
   },
   {
-    "revision": "840cbb2a618bda4c23d6",
-    "url": "/bozorcom/static/css/main.a09e0509.chunk.css"
+    "revision": "5ff4c4b3c5370193a5c0",
+    "url": "/bozorcom/static/css/main.94fb0bad.chunk.css"
   },
   {
     "revision": "c076aea4f6ddfbc8b97d",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/bozorcom/static/js/2.acde7f3f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "840cbb2a618bda4c23d6",
+    "revision": "5ff4c4b3c5370193a5c0",
     "url": "/bozorcom/static/js/main.59056bfa.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bozorcom/precache-manifest.eb57a01805323bd7136fe99af90e8a8a.js"
+  "/bozorcom/precache-manifest.6dd9560fe949ca5ae8ff61d230dafc82.js"
 );
 
 self.addEventListener('message', (event) => {
